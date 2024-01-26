599b4edb1b0b99dd61e0d783d1db8c89529a5728
Adrien <adrien@genei.io>
2024-01-26 10:37:58 +0000

    chore: cross-sentence emphasis

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import markdown from '@textlint/markdown-to-ast'
 
 function main() {
-  const ast = markdown.parse('Hello World!')
+  const ast = markdown.parse('Hi my name is *Adrien. Welcome* to my crib!')
   console.log(JSON.stringify(ast, null, 2))
 }
 
